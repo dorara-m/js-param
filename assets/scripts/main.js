6c81118b3efa5f0f943592a0722e8f2f8556431e
dorara@work <satosinisemono@gmail.com>
2019-10-09 17:32:53 +0900

    init

---
+++ assets/scripts/main.js
@@ -0,0 +1,62 @@
+var myXml = new XMLHttpRequest();
+myXml.onreadystatechange = function() {
+  if (myXml.readyState === 4) {
+    if (myXml.status == 200) {
+      console.log("OK"); //通信成功時
+    } else {
+      console.log("NO"); //通信失敗時
+    }
+  }
+}
+myXml.onload = function(e) {
+  console.log("complete"); //通信完了時
+  var data = JSON.parse(this.response);
+  var dataAry = [];
+  for (let key of Object.keys(data)) {
+    dataAry.push(data[key]);
+  }//json->配列に整形
+  var urlParam = location.search.substring(1);
+  if (urlParam) {//Paramがあれば下層に
+    switchPage(urlParam, dataAry);
+  } else {
+    makeList(dataAry);
+  }
+}
+myXml.open("GET", "/assets/scripts/data.json", true);
+myXml.send(null);
+
+function makeList(dataAry) {
+  var html = "<ul>";
+  for (var i = 0; i < dataAry.length; i++) {
+    html += "<li>";
+    html += "<p>" + dataAry[i].date + "</p>";
+    html += "<h3>" + dataAry[i].name + "</h3>";
+    html += '<p><a href="/?newsID='+ i +'">詳細 →</a></p>';
+    html += "</li>";
+  }
+  html += "</ul>";
+
+  var target = document.getElementById("list");
+  target.innerHTML = html;
+}
+
+function switchPage(urlParam, data) {
+  var param = urlParam.split('&');
+  var paramArray = [];
+  for (var i = 0; i < param.length; i++) {
+    var paramItem = param[i].split('=');
+    paramArray[paramItem[0]] = paramItem[1];
+  }
+  var paramID = paramArray.newsID;
+  if (data.length > Number(paramID)) {//有効なidかどうかチェック
+    var pageData = data[Number(paramID)];
+    var html = '<div class="page">'
+    html += '<h1>' + pageData.name + '</h1>'
+    html += '<p>' + pageData.date + '</p>'
+    html += '<p>' + pageData.text + '</p>'
+    html += '<p><a href="/">← 一覧に戻る</p>'
+    html += '</div>'
+    var target = document.getElementById("text");
+    target.innerHTML = html;
+  }
+}
